19b6300d956ef66bd668d2aa167ec821364b3bee
AD0410 <duque.duquegarcia@gmail.com>
2019-03-11 22:33:00 -0500

    temporal file selector view implementation

--- app/src/main/java/co/condorlabs/customcomponents/fileselector/FileSelectorView.kt
+++ app/src/main/java/co/condorlabs/customcomponents/fileselector/FileSelectorView.kt
@@ -2,19 +2,83 @@ package co.condorlabs.customcomponents.fileselector
 
 import android.content.Context
 import android.support.constraint.ConstraintLayout
+import android.support.v7.widget.AppCompatImageView
+import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
 import co.condorlabs.customcomponents.R
 
-class FileSelectorView : ConstraintLayout {
+class FileSelectorView @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    constructor(context: Context) : super(context) {}
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+    private var mConstraintLayoutTapButton: ConstraintLayout? = null
+    private var mImageViewIcon: AppCompatImageView? = null
+    private var mTextViewTitleTabButton: AppCompatTextView? = null
+    private var mTextViewSubtitleTabButton: AppCompatTextView? = null
+    private var mTextViewTextTabButton: AppCompatTextView? = null
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.file_selector_view, this)
+        setContentView()
+
+        findViewsInLayoutresourceFile()
+
+        attrs?.let { setupAttributeSet(it) }
+
+        mConstraintLayoutTapButton?.setOnClickListener {
+            Toast.makeText(context, "Sirve!!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun setContentView() {
+        LayoutInflater.from(context).inflate(R.layout.file_selector_view, this, true)
+    }
+
+    private fun findViewsInLayoutresourceFile() {
+        mConstraintLayoutTapButton = findViewById(R.id.containerLayout)
+        mImageViewIcon = findViewById(R.id.iconImageView)
+        mTextViewTitleTabButton = findViewById(R.id.titleTextView)
+        mTextViewSubtitleTabButton = findViewById(R.id.subtitleTextView)
+        mTextViewTextTabButton = findViewById(R.id.textTapTextView)
+    }
+
+    private fun setupAttributeSet(atributes: AttributeSet) {
+        val attrsArray = context.obtainStyledAttributes(atributes, R.styleable.FileSelectorView)
+
+        if (attrsArray.hasValue(R.styleable.FileSelectorView_src_tap_button)) {
+            attrsArray.getResourceId(R.styleable.FileSelectorView_src_tap_button, -1)?.let { imageResourceId ->
+                mImageViewIcon?.setImageResource(imageResourceId)
+            }
+        }
+
+        if (attrsArray.hasValue(R.styleable.FileSelectorView_tap_button_text)) {
+            attrsArray.getString(R.styleable.FileSelectorView_tap_button_text)?.let { text ->
+                mTextViewTextTabButton?.setText(text)
+            }
+        }
+
+        if (attrsArray.hasValue(R.styleable.FileSelectorView_tap_button_title)) {
+            attrsArray.getString(R.styleable.FileSelectorView_tap_button_title)?.let { title ->
+                mTextViewTitleTabButton?.apply {
+                    this.text = title
+                    visibility = View.VISIBLE
+                }
+            }
+        }
+
+        if (attrsArray.hasValue(R.styleable.FileSelectorView_tap_button_subtitle)) {
+            attrsArray.getString(R.styleable.FileSelectorView_tap_button_subtitle)?.let { subtitle ->
+                mTextViewSubtitleTabButton?.apply {
+                    this.text = subtitle
+                    visibility = View.VISIBLE
+                }
+            }
+        }
+
+        attrsArray.recycle()
     }
 }
---
+++ test/src/androidTest/java/co/condorlabs/customcomponents/test/FileSelectorViewTest.kt
@@ -0,0 +1,34 @@
+package co.condorlabs.customcomponents.test
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.matcher.ViewMatchers.withHint
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class FileSelectorViewTest : MockActivityTest() {
+
+    @Before
+    fun setup() {
+        MockActivity.layout = R.layout.activity_file_selector_view_test
+    }
+
+    @Test
+    fun shouldHaveDefaultHint() {
+        restartActivity()
+
+        //Given
+        val view = onView(withId(R.id.myCustomLayoutSelector))
+
+        //When
+        view.perform(click())
+
+        //Then
+        withHint("Phone").matches(view)
+    }
+}
