0fac02a7811eee28cb7f316fa1f24c5a029d61b8
Leidy Zuluaga <leidyzuluaga@Leidys-MacBook-Pro.local>
2019-03-14 13:50:25 -0500

    fix multiline

--- test/src/androidTest/java/co/condorlabs/customcomponents/test/BaseEditTextFieldTest.kt
+++ test/src/androidTest/java/co/condorlabs/customcomponents/test/BaseEditTextFieldTest.kt
@@ -26,6 +26,7 @@ import android.support.test.runner.AndroidJUnit4
 import co.condorlabs.customcomponents.customedittext.BaseEditTextFormField
 import co.condorlabs.customcomponents.helper.VALIDATE_EMPTY_ERROR
 import co.condorlabs.customcomponents.helper.VALIDATE_INCORRECT_ERROR
+import co.condorlabs.customcomponents.test.util.isTextDisplayed
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -64,6 +65,26 @@ class BaseEditTextFieldTest : MockActivityTest() {
 
         //Then
         Assert.assertEquals("Zip", formField?.hint)
+    }
+
+    @Test
+    fun shouldDisplayTextWithMultiline() {
+        MockActivity.layout = R.layout.activity_baseedittextfield_multiline_test
+        restartActivity()
+
+        //Given
+        val editText = Espresso.onView(withId(R.id.etBase))
+
+        //When
+        editText.perform(typeText("Amy normally hated Monday mornings, but this year was different. Kamal was in her art class and she liked Kamal. She was waiting outside the classroom when her friend Tara arrived."))
+
+        //Then
+        isTextDisplayed(
+            "Amy normally hated Monday mornings, but this year was" +
+                    " different. Kamal was in her art class and she liked" +
+                    " Kamal. She was waiting outside the classroom when he" +
+                    " friend Tara arrived."
+        )
     }
 
     @Test
