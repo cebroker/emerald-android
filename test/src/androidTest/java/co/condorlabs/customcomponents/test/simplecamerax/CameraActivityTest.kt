65bcdab1a63cc4af1fc1e461bc51c92cb1ad49ce
E Alexis Duque <eduque@condorlabs.io>
2019-11-22 12:22:22 -0500

    test again

--- test/src/androidTest/java/co/condorlabs/customcomponents/test/simplecamerax/CameraActivityTest.kt
+++ test/src/androidTest/java/co/condorlabs/customcomponents/test/simplecamerax/CameraActivityTest.kt
@@ -103,7 +103,7 @@ class CameraActivityTest {
 
             // When
             ruleActivity.runOnUiThread { cropButton.performClick() }
-            Thread.sleep(1000)
+            ruleActivity.activity.finish()
 
             // Then
             val byteArray = ruleActivity.activityResult.resultData.getByteArrayExtra(CAMERA_TAKE_PHOTO_PARAM)
