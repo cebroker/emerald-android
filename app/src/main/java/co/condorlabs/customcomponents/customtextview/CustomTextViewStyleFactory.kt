f2db6584f14f6ad202515fb4e8d2eb3952e26406
Laura <ldelarosa@condorlabs.io>
2021-02-10 15:17:52 -0500

    create SectionBodySmallTextViewStyle in array

--- app/src/main/java/co/condorlabs/customcomponents/customtextview/CustomTextViewStyleFactory.kt
+++ app/src/main/java/co/condorlabs/customcomponents/customtextview/CustomTextViewStyleFactory.kt
@@ -15,6 +15,7 @@ object CustomTextViewStyleFactory {
         put(H2_TITLE_TYPE, CustomTextViewStyle.H2TitleTextViewStyle)
         put(H1_TITLE_TYPE, CustomTextViewStyle.H1TitleTextViewStyle)
         put(H3_TITLE_TYPE, CustomTextViewStyle.H3TitleTextViewStyle)
+        put(BODY_SMALL_TYPE, CustomTextViewStyle.BodySmallTextViewStyle)
     }
 
     fun getStyleFromType(type: Int): CustomTextViewStyle {
