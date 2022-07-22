2b485b4fbb477be20c1a6ee2fa0c8f99e24e6e1b
Javier Santiago <javier.santiago@condorlabs.io>
2022-07-22 16:33:04 -0500

    Currency format funtionality

--- test/src/main/java/co/condorlabs/customcomponents/test/MockActivity.kt
+++ test/src/main/java/co/condorlabs/customcomponents/test/MockActivity.kt
@@ -24,16 +24,12 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class MockActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout)
-
     }
 
     companion object {
         var layout: Int = R.layout.activity_mock
     }
-
 }
-
